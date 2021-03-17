93c863e0103744bba8586ef88ca67e38f3ec9be3
safidimanjaka <n.andriampisaina@etechconsulting-mg.com>
2021-03-17 11:52:14 +0300

    json-server

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3200;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "ReserveBetSlip/Reserve": "/Reserve",
+  "FEWFixture/Sports": "/Sports",
+  "FEWHome/Upcoming?Culture=:culture&StartRecord=:startRecord": "/Upcoming",
+  "Betslip/Place": "/Place",
+  "FEWBetSlip/History": "/History",
+  "FEMobile/GetGroupedMatches": "/GetGroupedMatches",
+  "FEMobile/GetMatchPlayerOdds": "/GetMatchPlayerOdds",
+  "FEMobile/FixturesMenu": "/FixturesMenu",
+  "FEMobile/GetMatchTabs": "/GetMatchTabs",
+  "FEWBetslip/CalculateBonus": "/CalculateBonus",
+  "ReserveBetSlip/Retrieve": "/Retrieve"
+}))
 server.use(router);
 
 server.listen(port);
